71c581687d452a820ae175ede0dbefea2b877dea
Chris Smith <chris@dmdirc.com>
2010-01-26 05:02:53 +0000

    Graph improvements
    Closes #2

--- website/data.php
+++ website/data.php
@@ -26,15 +26,29 @@
   $g = imagecolorallocate($im, 0, 255, 0);
   $b = imagecolorallocate($im, 0, 0, 255);
 
-  $minx = min($datax); $maxx = max($datax); $rangex = max(1, $maxx - $minx);
-  $miny = min($datay); $maxy = max($datay); $rangey = max(1, $maxy - $miny);
-  $minz = min($dataz); $maxz = max($dataz); $rangez = max(1, $maxz - $minz);
+  $grey = imagecolorallocate($im, 200, 200, 200);
+  $lgrey = imagecolorallocate($im, 230, 230, 230);
+ 
+  $minx = min($datax); $maxx = max($datax); $rangex = max(0.01, $maxx - $minx);
+  $miny = min($datay); $maxy = max($datay); $rangey = max(0.01, $maxy - $miny);
+  $minz = min($dataz); $maxz = max($dataz); $rangez = max(0.01, $maxz - $minz);
   $lastx = $lasty = $lastz = -0;
 
+  imagestring($im, 1, 2, 6, 'Min: ' . $minx . '; Max: ' . $maxx . '; Range: ' . $rangex, $grey);
+  imagestring($im, 1, 2, 116, 'Min: ' . $miny . '; Max: ' . $maxy . '; Range: ' . $rangey, $grey);
+  imagestring($im, 1, 2, 226, 'Min: ' . $minz . '; Max: ' . $maxz . '; Range: ' . $rangez, $grey);
+
+  imageline($im, 0, 5, count($datax) * 2, 5, $grey);
+  imageline($im, 0, 105, count($datax) * 2, 105, $grey);
+  imageline($im, 0, 115, count($datax) * 2, 115, $grey);
+  imageline($im, 0, 215, count($datax) * 2, 215, $grey);
+  imageline($im, 0, 225, count($datax) * 2, 225, $grey);
+  imageline($im, 0, 325, count($datax) * 2, 325, $grey);
+
   for ($i = 0; $i < count($datax); $i++) {
-   $x = ($datax[$i] - $minx) * 200 / $rangex;
-   $y = ($datay[$i] - $miny) * 200 / $rangey + 100;
-   $z = ($dataz[$i] - $minz) * 200 / $rangez + 200;
+   $x = ($datax[$i] - $minx) * 100 / $rangex + 5;
+   $y = ($datay[$i] - $miny) * 100 / $rangey + 115;
+   $z = ($dataz[$i] - $minz) * 100 / $rangez + 225;
 
    if ($i > 0) {
     imageline($im, 2 * $i - 2, $lastx, $i * 2, $x, $r);
