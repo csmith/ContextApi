180f8420686df5ac0e261f11e377d2c77f79dba9
Chris Smith <chris@dmdirc.com>
2010-03-02 05:51:13 +0000

    Initial IBK-based model
    Analyser now outputs ARFF files. Closes #42.

--- code/Extractor/src/uk/co/md87/dsp/extractor/test/TrainingDataAnalyser.java
+++ code/Extractor/src/uk/co/md87/dsp/extractor/test/TrainingDataAnalyser.java
@@ -7,6 +7,8 @@ package uk.co.md87.dsp.extractor.test;
 
 import com.dmdirc.util.MapList;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -25,6 +27,7 @@ public class TrainingDataAnalyser {
 
     public static void main(final String ... args) throws IOException {
         final TrainingDataImporter importer = new TrainingDataImporter(args[0]);
+        final String type = args.length > 1 ? args[1] : "";
 
         final MapList<String, List<float[]>> data = importer.getTrainingData();
         final Set<Feature> features = new HashSet<Feature>();
@@ -35,17 +38,60 @@ public class TrainingDataAnalyser {
         features.addAll(createFeatures(MedianFeature.class, 6));
         features.addAll(createFeatures(MeanFeature.class, 6));
 
-        for (String activity : data.keySet()) {
-            System.out.println(activity);
+        System.out.println("@RELATION activity");
+        System.out.println();
 
-            int i = 0, j = 0;
+        final List<String> featureNames = new ArrayList<String>();
+
+        for (Feature feature : features) {
+            featureNames.add(feature.getName());
+        }
+
+        Collections.sort(featureNames);
+
+        for (String name : featureNames) {
+            System.out.println("@ATTRIBUTE \"" + name +"\" numeric");
+        }
+
+        System.out.print("@ATTRIBUTE classification {");
+
+        boolean first = true;
+        for (String activity : data.keySet()) {
+            if (first) {
+                first = false;
+            } else {
+                System.out.print(",");
+            }
+
+            System.out.print(activity);
+        }
+
+        System.out.println("}");
+        System.out.println();
+        System.out.println("@DATA");
+
+        for (String activity : data.keySet()) {
+            int j = 0;
             for (List<float[]> dataset : data.get(activity)) {
-                i++;
-                j = 0;
 
                 for (Window window : new Windower(dataset).getWindows()) {
                     j++;
-                    System.out.println(i + "." + j + ": " + window.getFeatures(features));
+
+                    first = true;
+                    for (Float value : window.getFeatures(features).values()) {
+                        if (first) {
+                            first = false;
+                        } else {
+                            System.out.print(",");
+                        }
+
+                        System.out.print(value);
+                    }
+
+                    System.out.print(",");
+                    System.out.print(!type.equals(activity)
+                            || (type.equals(activity) && j % 3 > 0) ? activity : "?");
+                    System.out.println();
                 }
             }
         }
