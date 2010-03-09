ff89f3b4d6083891720f27b02fb5fcc40ba45de5
Chris Smith <chris@dmdirc.com>
2010-03-09 13:38:54 +0000

    Fix force close

--- code/SensorLogger/src/uk/co/md87/android/sensorlogger/activities/IntroActivity.java
+++ code/SensorLogger/src/uk/co/md87/android/sensorlogger/activities/IntroActivity.java
@@ -6,12 +6,15 @@
 package uk.co.md87.android.sensorlogger.activities;
 
 import android.content.Intent;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
 import android.os.RemoteException;
+import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import uk.co.md87.android.sensorlogger.ExceptionHandler;
 
 import uk.co.md87.android.sensorlogger.R;
 
@@ -26,9 +29,25 @@ public class IntroActivity extends BoundActivity implements OnClickListener {
     public void onCreate(final Bundle icicle) {
         super.onCreate(icicle);
 
+        Thread.setDefaultUncaughtExceptionHandler(
+                new ExceptionHandler("http://chris.smith.name/android/upload",
+                getVersionName(), getIMEI()));
+
         setContentView(R.layout.intro);
 
         ((Button) findViewById(R.id.introstart)).setOnClickListener(this);
+    }
+
+    public String getVersionName() {
+        try {
+            return getPackageManager().getPackageInfo(getPackageName(), 0).versionName;
+        } catch (NameNotFoundException ex) {
+            return "Unknown";
+        }
+    }
+
+    public String getIMEI() {
+        return ((TelephonyManager) getSystemService(TELEPHONY_SERVICE)).getDeviceId();
     }
 
     /** {@inheritDoc} */
