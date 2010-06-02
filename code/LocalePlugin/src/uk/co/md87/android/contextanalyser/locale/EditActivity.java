0fac20b860c8fe87960d174a1d381d89aca6a182
Chris Smith <chris@dmdirc.com>
2010-06-02 12:34:02 +0100

    Flesh out edit activity

--- code/LocalePlugin/src/uk/co/md87/android/contextanalyser/locale/EditActivity.java
+++ code/LocalePlugin/src/uk/co/md87/android/contextanalyser/locale/EditActivity.java
@@ -23,12 +23,123 @@
 package uk.co.md87.android.contextanalyser.locale;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.twofortyfouram.SharedResources;
 
 /**
- * Activity which allows editing of the Locale trigger.
+ * This is the "Edit" activity for the locale plugin.
  *
  * @author chris
  */
 public class EditActivity extends Activity {
 
+    private static final int MENU_SAVE = 1;
+    private static final int MENU_DONT_SAVE = 2;
+
+    private boolean isCancelled;
+
+    @Override
+    public void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.main);
+
+        final String breadcrumbString = getIntent()
+                .getStringExtra(com.twofortyfouram.Intent.EXTRA_STRING_BREADCRUMB);
+        setTitle(String.format("%s%s%s", breadcrumbString,
+                com.twofortyfouram.Intent.BREADCRUMB_SEPARATOR,
+                getString(R.string.plugin_name)));
+       
+        ((LinearLayout) findViewById(R.id.frame))
+                .setBackgroundDrawable(SharedResources.getDrawableResource(getPackageManager(),
+                SharedResources.DRAWABLE_LOCALE_BORDER));
+
+        if (savedInstanceState == null) {
+            final Bundle forwardedBundle = getIntent()
+                    .getBundleExtra(com.twofortyfouram.Intent.EXTRA_BUNDLE);
+
+            if (forwardedBundle != null) {
+                ((TextView) findViewById(R.id.textview)).setText(forwardedBundle.getString("activity"));
+            }
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void finish() {
+        if (isCancelled) {
+            setResult(RESULT_CANCELED);
+        } else {
+            final Intent returnIntent = new Intent();
+
+            final Bundle storeAndForwardExtras = new Bundle();
+
+            storeAndForwardExtras.putString("activity",
+                    ((TextView) findViewById(R.id.textview)).getText().toString());
+            returnIntent.putExtra(com.twofortyfouram.Intent.EXTRA_STRING_BLURB,
+                    ((TextView) findViewById(R.id.textview)).getText().toString());
+
+            returnIntent.putExtra(com.twofortyfouram.Intent.EXTRA_BUNDLE, storeAndForwardExtras);
+            setResult(RESULT_OK, returnIntent);
+        }
+
+        super.finish();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean onCreateOptionsMenu(final Menu menu) {
+        super.onCreateOptionsMenu(menu);
+
+        final PackageManager manager = getPackageManager();
+
+        final Intent helpIntent = new Intent(com.twofortyfouram.Intent.ACTION_HELP);
+        helpIntent.putExtra(com.twofortyfouram.Intent.EXTRA_STRING_HELP_URL,
+                "http://chris.smith.name/android/contextanalyser/");
+
+        helpIntent.putExtra(com.twofortyfouram.Intent.EXTRA_STRING_BREADCRUMB, getTitle());
+
+        menu.add(SharedResources.getTextResource(manager, SharedResources.STRING_MENU_HELP))
+                .setIcon(SharedResources.getDrawableResource(manager,
+                SharedResources.DRAWABLE_MENU_HELP)).setIntent(helpIntent);
+        menu.add(0, MENU_DONT_SAVE, 0, SharedResources.getTextResource(manager,
+                SharedResources.STRING_MENU_DONTSAVE)).setIcon(SharedResources
+                .getDrawableResource(manager, SharedResources.DRAWABLE_MENU_DONTSAVE)).getItemId();
+        menu.add(0, MENU_SAVE, 0, SharedResources.getTextResource(manager,
+                SharedResources.STRING_MENU_SAVE)).setIcon(SharedResources
+                .getDrawableResource(manager, SharedResources.DRAWABLE_MENU_SAVE)).getItemId();
+
+        return true;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean onMenuItemSelected(final int featureId, final MenuItem item) {
+        switch (item.getItemId()) {
+            case MENU_SAVE: {
+                finish();
+                return true;
+            }
+            case MENU_DONT_SAVE: {
+                isCancelled = true;
+                finish();
+                return true;
+            }
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
--- code/LocalePlugin/src/uk/co/md87/android/contextanalyser/locale/R.java
+++ code/LocalePlugin/src/uk/co/md87/android/contextanalyser/locale/R.java
@@ -13,19 +13,24 @@ public final class R {
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
+    public static final class id {
+        public static final int frame=0x7f050000;
+        public static final int textview=0x7f050001;
+    }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
-        public static final int plugin_dialog_install=0x7f040002;
+        public static final int plugin_dialog_install=0x7f040003;
         /**  "Install" should match up with the plugin_dialog_install string 
          */
-        public static final int plugin_dialog_market_not_available=0x7f040005;
-        public static final int plugin_dialog_need_locale_informative=0x7f040004;
-        public static final int plugin_dialog_need_locale_message=0x7f040003;
+        public static final int plugin_dialog_market_not_available=0x7f040006;
+        public static final int plugin_dialog_need_locale_informative=0x7f040005;
+        public static final int plugin_dialog_need_locale_message=0x7f040004;
         /**  NOTE: these names cannot be changed without also changing the string constants in com.twofortyfouram.MarketActivity.  You will NOT get a compile time error 
          */
-        public static final int plugin_dialog_title=0x7f040001;
+        public static final int plugin_dialog_title=0x7f040002;
+        public static final int plugin_name=0x7f040001;
     }
 }
