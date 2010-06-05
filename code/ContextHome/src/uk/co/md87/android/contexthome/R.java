f0036cfabf42b562419b20ae6f8a08efd2aafde3
Chris Smith <chris@dmdirc.com>
2010-06-05 19:21:52 +0100

    Proper name and icon

--- code/ContextHome/src/uk/co/md87/android/contexthome/R.java
+++ code/ContextHome/src/uk/co/md87/android/contexthome/R.java
@@ -10,10 +10,13 @@ package uk.co.md87.android.contexthome;
 public final class R {
     public static final class attr {
     }
+    public static final class drawable {
+        public static final int icon=0x7f020000;
+    }
     public static final class layout {
-        public static final int main=0x7f020000;
+        public static final int main=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f030000;
+        public static final int app_name=0x7f040000;
     }
 }
